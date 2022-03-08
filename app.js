1ffe9612886afe97d4a9fdf35df0cebd2ef6fee8
Siam Islam <30402883+siamislam1603@users.noreply.github.com>
2022-03-08 17:57:37 +0600

    Adding a 404 page if no url matches

--- app.js
+++ app.js
@@ -5,7 +5,11 @@ const adminRouter=require('./routes/admin');
 const shopRouter=require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended:false})); // this will parse the form inputs to the object
-app.use(adminRouter);
 app.use(shopRouter);
+app.use(adminRouter);
+
+app.use((req,res,next)=>{
+    res.status(404).send('<h1>Page not found</h1>');
+})
 
 app.listen(3000);
