4da7ce39dadaf1476e7092b4302ba4bb593e74d5
Siam Islam <30402883+siamislam1603@users.noreply.github.com>
2022-03-09 18:36:06 +0600

    Used external files in the html by including static files folder & added templating engine pug for dynamic content showing

--- app.js
+++ app.js
@@ -2,12 +2,15 @@ const express=require('express');
 const bodyParser=require('body-parser');
 const app=express();
 const adminRouter=require('./routes/admin');
-const shopRouter=require('./routes/shop');
+const shopModule=require('./routes/shop');
 const path=require('path');
 
 app.use(bodyParser.urlencoded({extended:false})); // this will parse the form inputs to the object
-app.use(shopRouter);
+app.use(express.static(path.join(__dirname,'public')));
+app.use(shopModule.router);
 app.use(adminRouter);
+app.set('view engine','pug');
+app.set('views','views'); // by default views means process.cwd() + '/views'
 
 app.use((req,res,next)=>{
     res.status(404).sendFile(path.join(__dirname,'views','404err.html'));
--- routes/shop.js
+++ routes/shop.js
@@ -2,13 +2,16 @@ const express=require('express');
 const router=express.Router();
 const path=require('path');
 
+const products=[];
 router.get('/add-product',(req,res,next)=>{
     console.log('this is the 1st middleware');
-    res.sendFile(path.join(__dirname,'../','views','add-product.html'));
+    res.render('add-product');
     // next();
 });
 router.post('/add-product',(req,res,next)=>{
-    console.log('posted data',req.body);
+    products.push({title:req.body.title});
+    console.log('posted data',products);
     res.redirect('/');
 });
-module.exports=router;
+
+module.exports={router,products};
