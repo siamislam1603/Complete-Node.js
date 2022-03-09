1042947408b0e5c7c4caa3e377c689147f26094b
Siam Islam <30402883+siamislam1603@users.noreply.github.com>
2022-03-09 18:50:30 +0600

    Added dynamic contents to the root url on posting the form

--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,11 @@
 const express=require('express');
 const path=require('path');
+const shopModule=require('./shop');
 
 const router=express.Router();
 
 router.get('/',(req,res,next)=>{
-    res.render('shop');
+    res.render('shop',{products:shopModule.products});
 });
 
 module.exports=router;
