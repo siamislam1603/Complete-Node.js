aa690cb9c08569b190670a64dd08177984dc2e5a
Siam Islam <30402883+siamislam1603@users.noreply.github.com>
2022-03-16 16:54:55 +0600

    Refactoring the file read code

--- models/product.js
+++ models/product.js
@@ -3,31 +3,30 @@ const {dirname,join}=require('path');
 const rootDir=dirname(require.main.filename);
 const productsJson=join(rootDir,'data','products.json');
 
+const getProducts=(cb)=>{
+    fs.readFile(productsJson,(err,fileContent)=>{
+        if(err || fileContent.toString().length===0){
+            cb([]);
+        }
+        else{
+            const data=JSON.parse(fileContent);
+            cb(data);
+        }
+    });
+}
 module.exports= class Product{
     constructor(title){
         this.title=title;
     }
     save(){
-        fs.readFile(productsJson,(err,fileContent)=>{
-            console.log('adfkjdakL:',fileContent.toString(),this);
-            let data=[];
-            if(!err && fileContent.toString().length>0) 
-                data=JSON.parse(fileContent);
-            data.push({title:this.title});
-            fs.writeFile(productsJson,JSON.stringify(data),err=>{
+        getProducts(products=>{
+            products.push(this);
+            fs.writeFile(productsJson,JSON.stringify(products),err=>{
                 console.log('fdjakfj',err);
             });
         });
     }
     static fetchAll(cb){
-        fs.readFile(productsJson,(err,fileContent)=>{
-            if(err || fileContent.toString().length===0){
-                cb([]);
-            }
-            else{
-                const data=JSON.parse(fileContent);
-                cb(data);
-            }
-        });
+        getProducts(cb);
     }
 }
